a69220541abb591c292d97195ea1cab48db152a8
Alex Bashuk <a.bashuk@gmail.com>
2015-04-15 12:53:52 +0300

    Initial code

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+""" Optimal trajectory builder (Alex Bashuk's thesis work)."""
+
+# imports go here
+
+__author__ = "Alex Bashuk"
+__copyright__ = "Copyright (c) 2015 Alex Bashuk"
+__credits__ = ["Alex Bashuk"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Alex Bashuk"
+__email__ = "alex@bashuk.tk"
+__status__ = "Development"
+
+
+
+if __name__ == '__main__':
+    pass
